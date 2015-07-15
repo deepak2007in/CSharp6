8573e78352bc59dbef904bc5182f8e4344009271
Deepak Agnihotri <deepak2007in@gmail.com>
2015-07-15 16:53:23 +0530

    Null conditional operator with NameOf in INotifyPropertyChanged

---
+++ CSharp6/EvilPlanViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace CSharp6
+{
+    public class EvilPlanViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        private string evilGenius;
+        public string OwningEvilGenius
+        {
+            get
+            {
+                return evilGenius;
+            }
+            set
+            {
+                if(value != evilGenius)
+                {
+                    evilGenius = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(OwningEvilGenius)));
+                }
+            }
+        }
+    }
+}
