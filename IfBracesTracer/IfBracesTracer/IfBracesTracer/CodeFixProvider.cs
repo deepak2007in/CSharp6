1f1f928aa8f50370d2a7d5a0ea52dbc9c87cd62b
Deepak Agnihotri <deepak2007in@gmail.com>
2015-07-20 18:31:11 +0530

    Initial draft of the CodeFix

--- IfBracesTracer/IfBracesTracer/IfBracesTracer/CodeFixProvider.cs
+++ IfBracesTracer/IfBracesTracer/IfBracesTracer/CodeFixProvider.cs
@@ -37,31 +37,23 @@ namespace IfBracesTracer
             var diagnosticSpan = diagnostic.Location.SourceSpan;
 
             // Find the type declaration identified by the diagnostic.
-            var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<TypeDeclarationSyntax>().First();
+            var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<IfStatementSyntax>().First();
 
             // Register a code action that will invoke the fix.
-            //context.RegisterCodeFix(
-            //    CodeAction.Create("Make uppercase", c => MakeUppercaseAsync(context.Document, declaration, c)),
-            //    diagnostic);
+            context.RegisterCodeFix(
+                CodeAction.Create("Add Deepak's Braces", c => AddBracesAsync(context.Document, declaration, c)),
+                diagnostic);
         }
 
-        private async Task<Solution> MakeUppercaseAsync(Document document, TypeDeclarationSyntax typeDecl, CancellationToken cancellationToken)
+        private async Task<Document> AddBracesAsync(Document document, IfStatementSyntax ifStatement, CancellationToken cancellationToken)
         {
-            // Compute new uppercase name.
-            var identifierToken = typeDecl.Identifier;
-            var newName = identifierToken.Text.ToUpperInvariant();
-
-            // Get the symbol representing the type to be renamed.
-            var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
-            var typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
-
-            // Produce a new solution that has all references to that type renamed, including the declaration.
-            var originalSolution = document.Project.Solution;
-            var optionSet = originalSolution.Workspace.Options;
-            var newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol, newName, optionSet, cancellationToken).ConfigureAwait(false);
-
-            // Return the new solution with the now-uppercase type name.
-            return newSolution;
+            var nonBlockedStatement = ifStatement.Statement as ExpressionStatementSyntax;
+            var newBlockedStatement = SyntaxFactory.Block(statements: nonBlockedStatement);
+            var newIfStatement = ifStatement.ReplaceNode(oldNode: nonBlockedStatement, newNode: newBlockedStatement);
+            var root = await document.GetSyntaxRootAsync();
+            var newRoot = root.ReplaceNode(oldNode: ifStatement, newNode: newIfStatement);
+            var newDocument = document.WithSyntaxRoot(newRoot);
+            return newDocument;
         }
     }
 }
