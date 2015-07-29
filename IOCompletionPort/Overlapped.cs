fedd2405ca7f2c80276ed154e2dd62b9af97f9ba
Deepak Agnihotri <deepak2007in@gmail.com>
2015-07-29 18:10:05 +0530

    Grab the understanding of the StructureLayout

---
+++ IOCompletionPort/Overlapped.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOCompletionPort
+{
+    [StructLayout(LayoutKind.Sequential, CharSet=CharSet.Auto)]
+    public struct Overlapped
+    {
+        // UInt32* ulpInternal;
+        // UInt32* ulpInternalHigh;
+        Int32 lOffset;
+        Int32 lOffsetHigh;
+        UInt32 hEvent;
+    }
+
+    
+}
--- IOCompletionPort/Program.cs
+++ IOCompletionPort/Program.cs
@@ -1,18 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace IOCompletionPort
 {
-    class Program
+    public class Program
     {
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr CreateFile(
+        [MarshalAs(UnmanagedType.LPTStr)] string filename,
+        [MarshalAs(UnmanagedType.U4)] FileAccess access,
+        [MarshalAs(UnmanagedType.U4)] FileShare share,
+        IntPtr securityAttributes,
+        [MarshalAs(UnmanagedType.U4)] FileMode creationDisposition,
+        [MarshalAs(UnmanagedType.U4)] FileAttributes flagsAndAttributes,
+        IntPtr templateFile);
+
         static void Main(string[] args)
         {
-            const uint Flags = 128 | (uint)1 << 30;
-            Console.WriteLine(Flags);
+            var instance = new UnionLike();
+            instance.value1 = 5;
+            instance.value2 = 10;
+            // instance.value3 = Double.MaxValue;
+            Console.WriteLine(instance.value1);
+            Console.WriteLine(instance.value2);
+            // Console.WriteLine(instance.value3);
             Console.ReadLine();
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct UnionLike
+        {
+            [FieldOffset(0)]
+            public int value1;
+            [FieldOffset(3)]
+            public int value2;
+            //[FieldOffset(0)]
+            //public double value3;
         }
     }
 }
