86b0b1d51b1c1b057bdc36f50cd3b5cb1a84b813
Deepak Agnihotri <deepak2007in@gmail.com>
2015-07-29 17:13:02 +0530

    Used bitwise logical operator

---
+++ IOCompletionPort/CreateFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOCompletionPort
+{
+    public class CreateFile
+    {
+        const uint Flags = 128 | (uint)1 << 30;
+
+    }
+}
---
+++ IOCompletionPort/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOCompletionPort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const uint Flags = 128 | (uint)1 << 30;
+            Console.WriteLine(Flags);
+            Console.ReadLine();
+        }
+    }
+}
