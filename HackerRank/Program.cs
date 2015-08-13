9a8d26ed0c322c98bdcb7c81550b69778f77db47
Deepak Agnihotri <deepak2007in@gmail.com>
2015-08-13 16:55:28 +0530

    Optimized algorithm for prime numbers

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class Solution
 {
     static void Main(string[] args)
     {
+        var started = DateTimeOffset.Now;
         var input = 100;
         Console.WriteLine(getNumberOfPrimes(input));
+        Console.WriteLine((DateTimeOffset.Now - started).TotalMilliseconds);
         input = 1000000;
         Console.WriteLine(getNumberOfPrimes(input));
+        Console.WriteLine((DateTimeOffset.Now - started).TotalMilliseconds);
         Console.ReadLine();
     }
 
@@ -18,20 +22,32 @@ class Solution
        Output 78498
      */
     static int getNumberOfPrimes(int n) {
-        var numberOfPrime = 0;
-        for (var actual = 2; actual < n; actual++) {
-            var isPrime = true;
-            for (var devisor = actual - 1; devisor > 1; devisor--) {
-                if (actual % devisor == 0)
+        var primeNumbers = new List<int>(new[] { 2, 3, 5 });
+        if (n > 5)
+        {
+            for (var actual = 6; actual < n; actual++)
+            {
+                var lastDigit = actual % 10;
+                if (lastDigit % 2 == 0 || lastDigit % 5 == 0)
                 {
-                    isPrime = false;
-                    break;
+                    continue;
+                }
+                var isPrime = true;
+                for (var index = 0; index < primeNumbers.Count; index++)
+                {
+                    if (actual % primeNumbers[index] == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    primeNumbers.Add(actual);
                 }
             }
-            if (isPrime) {
-                numberOfPrime++;
-            }
         }
-        return numberOfPrime;
+
+        return n < 6 ? 3 : primeNumbers.Count;
     }
 }
