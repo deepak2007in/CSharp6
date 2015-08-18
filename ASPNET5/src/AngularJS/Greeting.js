80e555c2c4a5c95ddd184e6e5cce3ac5770c0a88
Deepak Agnihotri <deepak2007in@gmail.com>
2015-08-18 20:41:58 +0530

    Adding behaviours to the controller

--- ASPNET5/src/AngularJS/Greeting.js
+++ ASPNET5/src/AngularJS/Greeting.js
@@ -1,4 +1,5 @@
 ﻿var myApp = angular.module('myApp', []);
 myApp.controller('GreetingController', ['$scope', function ($scope) {
     $scope.greeting = "Hola!";
+    $scope.double = function (value) { return value * 2; };
 }]);
