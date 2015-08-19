b7ed678a225cfa1c3cb0a59bf98cc34067b5fe58
Deepak Agnihotri <deepak2007in@gmail.com>
2015-08-19 13:38:29 +0530

    Using provider to get the service instance

--- ASPNET5/src/AngularJS/Greeting.js
+++ ASPNET5/src/AngularJS/Greeting.js
@@ -3,14 +3,16 @@ myApp.controller('MyController', ['$scope', 'notify', function ($scope, notify) 
     $scope.callNotify = function (msg) {
         notify(msg);
     };
-}]).
-factory('notify', ['$window', function (win) {
-    var msgs = [];
-    return function (msg) {
-        msgs.push(msg);
-        if (msgs.length == 3) {
-            win.alert(msgs.join("\n"));
-            msgs = [];
-        }
-    };
+}]);
+myApp.config(['$provide', function ($provide) {
+    $provide.factory('notify', ['$window', function (win) {
+        var msgs = [];
+        return function (msg) {
+            msgs.push(msg);
+            if (msgs.length == 3) {
+                win.alert(msgs.join("\n"));
+                msgs = [];
+            }
+        };
+    }]);
 }]);
