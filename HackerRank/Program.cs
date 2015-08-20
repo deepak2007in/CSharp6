9b058b370e35293d203d76eed31710ea8a723668
Deepak Agnihotri <deepak2007in@gmail.com>
2015-08-20 10:05:51 +0530

    Angry Professor

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -1,16 +1,20 @@
 ﻿using System;
+using System.Linq;
 
 class Solution
 {
     static void Main(string[] args)
     {
-        var input = decimal.Parse(Console.ReadLine());
-        var output = input;
-        for (decimal i = input - 1; i > 1; i--)
+        var totalNumberOfTestCase = int.Parse(Console.ReadLine());
+        for (int i = 0; i < totalNumberOfTestCase; i++)
         {
-            output = output * i;
+            var passingCondition = Console.ReadLine();
+            var passingExpectation = int.Parse(passingCondition.Split(' ')[1]);
+            var inputValues = Console.ReadLine().Split(' ').Select(num => int.Parse(num));
+            var passingActual = inputValues.Where(num => num < 1).Count();
+            var isClassCancelled = (passingActual >= passingExpectation) ? "NO" : "YES";
+            Console.WriteLine(isClassCancelled);
         }
-        Console.WriteLine(output);
         Console.ReadLine();
     }
 }
