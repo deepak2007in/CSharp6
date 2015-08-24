e55ea82296d130f26445d68319ac8c54bc7e1e1f
Deepak Agnihotri <deepak2007in@gmail.com>
2015-08-24 17:21:15 +0530

    Bitwise And Operator

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -7,15 +7,10 @@ class Solution
     static Func[] funcArr = new Func[10];
     static void Main(string[] args)
     {
-        for (var i = 0; i < 10; i++)
-        {
-            int j = i;
-            funcArr[i] = delegate () { Console.WriteLine("{0}", j); };
-        }
-        for (var i = 0; i < funcArr.Length; i++)
-        {
-            funcArr[i]();
-        }
+        var i = 10;
+        var j = 9;
+        var k = i & j;
+        Console.WriteLine(k);
         Console.ReadLine();
     }
 }
