fd41b35823aeabe131ffa4a03811976bd150fde6
Deepak Agnihotri <deepak2007in@gmail.com>
2015-08-24 16:53:57 +0530

    Lexical Closure Test in C#

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -1,114 +1,20 @@
 ﻿using System;
 using System.Linq;
 
+delegate void Func();
 class Solution
 {
+    static Func[] funcArr = new Func[10];
     static void Main(string[] args)
     {
-        args = new[] { "4 4 1", "1 2 3 4", "5 6 7 8", "9 10 11 12", "13 14 15 16" };
-        var firstInput = args[0].Split(' ').Select(num => int.Parse(num.ToString())).ToArray();
-        var numberOfRows = firstInput[0];
-        var numberOfColumns = firstInput[1];
-        var numberOfRotation = firstInput[2];
-        var matrix = new int[numberOfRows, numberOfColumns];
-        for (int rowIndex = 0; rowIndex < numberOfRows; rowIndex++)
+        for (var i = 0; i < 10; i++)
         {
-            var line = args[rowIndex + 1].Split(' ').Select(num => int.Parse(num.ToString())).ToArray();
-            for (var columnIndex = 0; columnIndex < numberOfColumns; columnIndex++)
-            {
-                matrix[rowIndex, columnIndex] = line[columnIndex];
-            }
+            int j = i;
+            funcArr[i] = delegate () { Console.WriteLine("{0}", j); };
         }
-
-        var swappedMatrix = matrix.Clone() as int[,];
-        for (var rotation = 0; rotation < numberOfRotation; rotation++)
+        for (var i = 0; i < funcArr.Length; i++)
         {
-            for (int rowIndex = 0; rowIndex < numberOfRows; rowIndex++)
-            {
-                for (var columnIndex = 0; columnIndex < numberOfColumns; columnIndex++)
-                {
-                    if (rowIndex == 0)
-                    {
-                        if (columnIndex == 0)
-                        {
-                            swappedMatrix[rowIndex, columnIndex] = matrix[rowIndex, columnIndex + 1];
-                        }
-                        else if (columnIndex < numberOfColumns - 1)
-                        {
-                            swappedMatrix[rowIndex, columnIndex] = matrix[rowIndex, columnIndex + 1];
-                        }
-                        else if (columnIndex == numberOfColumns - 1)
-                        {
-                            swappedMatrix[rowIndex, columnIndex] = matrix[rowIndex + 1, columnIndex];
-                        }
-                    }
-                    else if (rowIndex < numberOfRows - 1)
-                    {
-                        if (columnIndex == 0)
-                        {
-                            swappedMatrix[rowIndex, columnIndex] = matrix[rowIndex - 1, columnIndex];
-                        }
-                        else if (columnIndex < numberOfColumns - 1)
-                        {
-                            if (rowIndex + 1 == numberOfRows - 1)
-                            {
-                                if (columnIndex + 1 == numberOfColumns - 1)
-                                {
-                                    swappedMatrix[rowIndex, columnIndex] = matrix[rowIndex, columnIndex - 1];
-                                }
-                                else
-                                {
-                                    swappedMatrix[rowIndex, columnIndex] = matrix[rowIndex - 1, columnIndex];
-                                }
-                            }
-                            else if (columnIndex + 1 == numberOfColumns - 1)
-                            {
-                                swappedMatrix[rowIndex, columnIndex] = matrix[rowIndex + 1, columnIndex];
-                            }
-                            else
-                            {
-                                if(rowIndex - 1 != 0)
-                                {
-                                    swappedMatrix[rowIndex, columnIndex] = matrix[rowIndex - 1, columnIndex];
-                                }
-                                else
-                                {
-                                    swappedMatrix[rowIndex, columnIndex] = matrix[rowIndex, columnIndex + 1];
-                                }
-                            }
-                        }
-                        else if (columnIndex == numberOfColumns - 1)
-                        {
-                            swappedMatrix[rowIndex, columnIndex] = matrix[rowIndex + 1, columnIndex];
-                        }
-                    }
-                    else if (rowIndex == numberOfRows - 1)
-                    {
-                        if (columnIndex == 0)
-                        {
-                            swappedMatrix[rowIndex, columnIndex] = matrix[rowIndex - 1, columnIndex];
-                        }
-                        else if (columnIndex < numberOfColumns - 1)
-                        {
-                            swappedMatrix[rowIndex, columnIndex] = matrix[rowIndex, columnIndex - 1];
-                        }
-                        else if (columnIndex == numberOfColumns - 1)
-                        {
-                            swappedMatrix[rowIndex, columnIndex] = matrix[rowIndex, columnIndex - 1];
-                        }
-                    }
-                }
-            }
-            matrix = swappedMatrix.Clone() as int[,];
-        }
-        for (int rowIndex = 0; rowIndex < numberOfRows; rowIndex++)
-        {
-            var output = string.Empty;
-            for (var columnIndex = 0; columnIndex < numberOfColumns; columnIndex++)
-            {
-                output += swappedMatrix[rowIndex, columnIndex].ToString() + ' ';
-            }
-            Console.WriteLine(output.Trim());
+            funcArr[i]();
         }
         Console.ReadLine();
     }
