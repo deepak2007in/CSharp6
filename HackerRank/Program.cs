162f5ee9b5754a5b554a8002b235e28a2268bd81
Deepak Agnihotri <deepak2007in@gmail.com>
2015-08-25 07:11:57 +0530

    Funny String Algorithm

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -4,15 +4,32 @@ class Solution
 {
     static void Main(string[] args)
     {
-        try
+        var total = int.Parse(Console.ReadLine());
+        for (var i = 0; i < total; i++)
         {
-            var i = 3;
-            var j = 0;
-            var k = i/j;
-        }
-        catch
-        {
-            Console.WriteLine("Exception");
+            var input = Console.ReadLine().ToCharArray();
+            var reverse = new char[input.Length];
+            input.CopyTo(reverse, 0);
+            Array.Reverse(reverse);
+            var isFunny = true;
+            for (var index = input.Length - 1; index > 0; index--)
+            {
+                var inputDifference = (int)input[index] - (int)input[index - 1];
+                var reverseDifference = (int)reverse[index] - (int)reverse[index - 1];
+                isFunny = (Math.Abs(inputDifference) == Math.Abs(reverseDifference));
+                if (!isFunny)
+                {
+                    break;
+                }
+            }
+            if (isFunny)
+            {
+                Console.WriteLine("Funny");
+            }
+            else
+            {
+                Console.WriteLine("Not Funny");
+            }
         }
         Console.ReadLine();
     }
